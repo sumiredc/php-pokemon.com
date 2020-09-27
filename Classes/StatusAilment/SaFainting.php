995f0e09fdadddca24381c546738f427303e4d81
阪本良介 <sakamoto@sumire-sakamoto.co.jp>
2020-09-27 21:35:52 +0900

    状態異常クラス

---
+++ Classes/StatusAilment/SaFainting.php
@@ -0,0 +1,14 @@
+<?php
+require_once(__DIR__.'/../StatusAilment.php');
+
+// ひんし
+class SaFainting extends StatusAilment
+{
+
+    /**
+    * 正式名称
+    * @var string
+    */
+    protected $name = 'ひんし';
+
+}
