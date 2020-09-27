2f95c515f362138e71695d45af1793abc8651605
阪本良介 <sakamoto@sumire-sakamoto.co.jp>
2020-09-27 21:35:27 +0900

    ポケモンクラス

---
+++ Classes/Pokemon/Mew.php
@@ -0,0 +1,58 @@
+<?php
+require_once(__DIR__.'/../Pokemon.php');
+
+// ミュウ
+class Mew extends Pokemon
+{
+
+    /**
+    * 正式名称
+    * @var string(min:1 max:5)
+    */
+    protected $name = 'ミュウ';
+
+    /**
+    * タイプ
+    * @var array
+    */
+    protected $types = ['Psychic'];
+
+    /**
+    * 初期レベル
+    * @var array
+    */
+    protected $default_level = [
+        5
+    ];
+
+    /**
+    * 基礎経験値
+    * @var integer
+    */
+    protected $base_exp = 300;
+
+    /**
+    * レベルアップで覚える技
+    * @var array
+    */
+    protected $level_move = [
+        [1, 'Growl'],
+        [1, 'FireSpin'],
+        [1, 'PoisonPowder'],
+        [1, 'ConfuseRay'],
+    ];
+
+    /**
+    * 種族値
+    * @var array
+    */
+    protected $base_stats = [
+        'HP' => 100,
+        'Attack' => 100,
+        'Defense' => 100,
+        'SpAtk' => 100,
+        'SpDef' => 100,
+        'Speed' => 100,
+    ];
+
+}
