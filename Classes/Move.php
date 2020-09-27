507dc9996f488362b5e3783414cd58fd8b09f5f8
阪本良介 <sakamoto@sumire-sakamoto.co.jp>
2020-09-27 21:35:15 +0900

    技クラス

---
+++ Classes/Move.php
@@ -0,0 +1,121 @@
+<?php
+require_once(__DIR__.'/../Traits/InstanceTrait.php');
+require_once(__DIR__.'/../Traits/ResponseTrait.php');
+
+// 技
+abstract class Move
+{
+    use InstanceTrait;
+    use ResponseTrait;
+
+    /**
+    * インスタンス作成時に実行される処理
+    *
+    * @return void
+    */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+    * 追加効果
+    *
+    * @return void
+    */
+    public function effects(...$args)
+    {
+        //
+    }
+
+    /**
+    * 名称の取得
+    *
+    * @return string
+    */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+    * 説明文の取得
+    *
+    * @return string
+    */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+    * タイプの取得
+    *
+    * @return object
+    */
+    public function getType()
+    {
+        return $this->getInstance($this->type);
+    }
+
+    /**
+    * 分類の取得
+    *
+    * @return string
+    */
+    public function getSpecies()
+    {
+        return $this->species;
+    }
+
+    /**
+    * 威力の取得
+    *
+    * @return string
+    */
+    public function getPower()
+    {
+        return $this->power;
+    }
+
+    /**
+    * 命中率の取得
+    *
+    * @return integer
+    */
+    public function getAccuracy()
+    {
+        return $this->accuracy;
+    }
+
+    /**
+    * 使用回数の取得
+    *
+    * @return integer
+    */
+    public function getPp()
+    {
+        return $this->pp;
+    }
+
+    /**
+    * 優先度の取得
+    *
+    * @return integer
+    */
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+
+    /**
+    * 急所ランクの取得
+    *
+    * @return integer
+    */
+    public function getCritical()
+    {
+        return $this->critical ?? 0;
+    }
+
+}
