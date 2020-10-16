7f7d3b99cdf66e8c589361523d14cd84bd9299f4
阪本良介 <sakamoto@sumire-sakamoto.co.jp>
2020-10-17 00:16:47 +0900

    経験値アニメーションの実装

--- Resources/Pages/Initial.php
+++ Resources/Pages/Initial.php
@@ -45,6 +45,7 @@ $controller = new InitialController();
                 <div class="row">
                     <div class="col-12">
                         <ul class="overflow-auto" style="height: 80px;">
+                            <li>2020/10/17 経験値取得時のアニメーションを追加しました</li>
                             <li>2020/10/12 PHPポケモンを公開しました</li>
                         </ul>
                     </div>
