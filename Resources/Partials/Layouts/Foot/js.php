f7b2f9c9a86bd287f2dd3d50ad4a5665df16d0ca
阪本良介 <sakamoto@sumire-sakamoto.co.jp>
2020-12-02 15:27:38 +0900

    トースター（toastr.js）の追加

--- Resources/Partials/Layouts/Foot/js.php
+++ Resources/Partials/Layouts/Foot/js.php
@@ -1,6 +1,7 @@
 <script type="text/javascript" src="/Assets/js/Lib/jquery-3.5.1.min.js"></script>
 <script type="text/javascript" src="/Assets/js/Lib/jquery-ui.min.js"></script>
-<script type="text/javascript" src="/Assets/js/Lib/jquery.keyframes.min.js"></script>
-<script type="text/javascript" src="/Assets/js/Lib/jquery.ui.touch-punch.min.js"></script>
 <script type="text/javascript" src="/Assets/js/Lib/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="/Assets/js/Lib/jquery.keyframes.min.js" defer></script>
+<script type="text/javascript" src="/Assets/js/Lib/jquery.ui.touch-punch.min.js" defer></script>
+<script type="text/javascript" src="/Assets/js/Lib/toastr.min.js" defer></script>
 <script type="text/javascript" src="/Assets/js/common.js" defer></script>
